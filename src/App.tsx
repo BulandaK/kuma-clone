14d1a1b8428730f6bba5f33f2674f661b718485e
Bulanda <kamilbulandus@gmail.com>
2024-02-05 15:52:04 +0100

    add color palette

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,27 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Layout from "./components/Layout/Layout";
 import StatusPage from "./pages/StatusPage/StatusPage";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0D1117",
+    },
+  },
+});
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Navigate to="/dashboard" />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/manage-status-page" element={<StatusPage />} />
-      </Route>
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/manage-status-page" element={<StatusPage />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   );
 }
 
